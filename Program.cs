59964d76990d4525872687cb346ca90dd6f761e9
Boya Pushpalatha <60311622+Pushpalatha76@users.noreply.github.com>
2021-10-14 19:02:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace Demo_Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linq Implementation in csharp");
+            int[] scores = new int[] { 20, 25, 30, 35, 40, 45, 50 };
+
+            IEnumerable<int> Scorequery =
+                from score in scores
+                where score > 35
+                select score;
+            foreach(var item in Scorequery)
+            {
+                Console.WriteLine("value:{0}\n", item);
+            }
+
+        }
+    }
+}
